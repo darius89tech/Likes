f07b0233c31c4e6a77773e5cff58726d08ad525a
darius washington <dariusqw89@gmail.com>
2023-07-13 16:29:33 -0400

    initial likes

---
+++ script.js
@@ -0,0 +1,23 @@
+var likes = document.querySelector("#Liked-1");
+var topCount = 0;
+
+function liked(9) {
+    topCount++;
+    likes.innerText = topCount + " like(s)";
+}
+
+var likes2 = document.querySelector("#Liked-2");
+var topCount2 = 0;
+
+function liked2(12) {
+    topCount2++;
+    likes2.innerText = topCount2 + " like(s)";
+}
+
+var likes3 = document.querySelector("#Liked-3");
+var topCount3 = 0;
+
+function liked3(9) {
+    topCount3++;
+    likes3.innerText = topCount3 + " like(s)";
+}
